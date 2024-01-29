c8e8d86e61ed4f667e5370d603cb7824d5351213
rosauromatib <rosauromatibagjr@gmail.com>
2024-01-29 09:54:39 +0800

    fix tuigrid.java for add row

--- toast-ui-grid/src/main/resources/META-INF/resources/frontend/src/views/toastuigrid/toast-ui-grid-view.tsx
+++ toast-ui-grid/src/main/resources/META-INF/resources/frontend/src/views/toastuigrid/toast-ui-grid-view.tsx
@@ -53,6 +53,7 @@ window.toastuigrid = {
         console.log("parsedItems: ", parsedItems);
         let parsedOptions = JSON.parse(optionsJson);
         let editingRowKey: string | number = -1;
+        console.log("Columns1: ", JSON.parse(parsedOptions.columns));
         const {columns, contextMenus, filterValues} = this.getColumns(container, JSON.parse(parsedOptions.columns));
         console.log("Columns: ", columns);
         let prevColumnName: string = "";
